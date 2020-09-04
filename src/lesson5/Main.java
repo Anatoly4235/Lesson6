b6c16196900240b3e05146d87704b8fbb418dc63
Anatoly <kosov4235@yandex.ru>
2020-09-04 11:24:10 +0300

    Домашнее задание. простое ООП

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -3,9 +3,31 @@ package lesson5;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        employee employee1 = new employee("Константин", "IT", "123@mail.ru", "8(926)111-22-33", 100000, 30);
 
+        employee employee2 = new employee("Виктор","IT", "123@mail.ru", "8(926)111-22-33", 100000, 41);
 
+        employee1.info();
 
+        employee2.info();
+        System.out.println();
+        System.out.println();
+
+        employee[] empArray = new employee[7];
+        empArray[0] = new employee("Тихонов Андрей", "Engineer", "tihonov@spartak.ru", "8(923)123-12-15", 100000, 35);
+        empArray[1] = new employee("Макаров Дмитрий", "Engineer", "dima@IT.com", "8(923)123-16-58", 80000, 33);
+        empArray[2] = new employee("Лютов Константин", "Bus Driver", "driver@mail.ru", "8(923)123-42-84", 30000, 37);
+        empArray[3] = new employee("Шкода Михаил", "Engineer", "misha@gmail.com", "8(923)123-32-91", 900000, 45);
+        empArray[4] = new employee("Лукашенко Александр", "TopManager", "lukash@mailbox.by", "88(923)123-11-99", 150000, 65);
+        empArray[5] = new employee("Бурый Виктор", "Security", "ivivan@mailbox.com", "8(923)123-37-37", 90000, 35);
+        empArray[6] = new employee("Бурденко Николай", "Doctor", "ivivan@mailbox.com", "8(923)123-28-61", 170000, 70);
+
+        System.out.println("Ниже перечисленны сотрудники старше 40 лет");
+        for (int i = 0; i < empArray.length; i++) {
+            if(empArray[i].age>=40){
+                System.out.println(empArray[i]);
+            }
+
+        }
     }
 }
--- src/lesson5/employee.java
+++ src/lesson5/employee.java
@@ -1,5 +1,36 @@
 package lesson5;
 
 public class employee {
-    
+
+    private String name;
+    private String position;
+    private String mail;
+    private String number;
+    private int wage;
+    public int age;
+
+    public employee(String name, String position, String mail, String number, int wage, int age) {
+        this.name = name;
+        this.position = position;
+        this.mail = mail;
+        this.number = number;
+        this.wage = wage;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.printf("Сотрудник %s, %s, %s, %s, %d, %d\n", name, position, mail, number, wage, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник{" +
+                "имя='" + name + '\'' +
+                ", позиция ='" + position + '\'' +
+                ", mail='" + mail + '\'' +
+                ", телефон='" + number + '\'' +
+                ", зарплата=" + wage +
+                ", возраст=" + age +
+                '}';
+    }
 }
